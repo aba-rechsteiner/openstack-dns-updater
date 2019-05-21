ea42c51326f83b7acc688136ae73ef99522e5aa2
Benjamin Rechsteiner <benjamin.rechsteiner@abacus.ch>
2019-05-21 22:15:33 +0200

    refactoring

--- openstack-dns-updater.py
+++ openstack-dns-updater.py
@@ -71,6 +71,10 @@ class DnsUpdater(ConsumerMixin):
         suggested_zone = api.servers[0].suggest_zone(name)
         return api.servers[0].get_zone(suggested_zone.name)
 
+    def get_reverse_pointer(self, ipaddr):
+        pointer = ipaddress.ip_address(ipaddr).reverse_pointer
+        return pointer + '.'
+
     def _handle_message(self, body):
         #log.debug(body)
         jbody = json.loads(body["oslo.message"])
@@ -93,10 +97,8 @@ class DnsUpdater(ConsumerMixin):
                         ipv4addr = fixed_ips1
                         ipv6addr = fixed_ips0
                     log.info("Adding {} {} {}".format(instancename, ipv4addr, ipv6addr))
-                    ptr_v4 = ipaddress.ip_address(ipv4addr).reverse_pointer
-                    ptr_v6 = ipaddress.ip_address(ipv6addr).reverse_pointer
-                    fq_ptr_v4 = ptr_v4 + '.'
-                    fq_ptr_v6 = ptr_v6 + '.'
+                    fq_ptr_v4 = get_reverse_pointer(ipv4addr)
+                    fq_ptr_v6 = get_reverse_pointer(ipv6addr)
                     ptr_v4_zone = self.suggested_zone(fq_ptr_v4)
                     ptr_v6_zone = self.suggested_zone(fq_ptr_v6)
                     ptr_v4_name = fq_ptr_v4.replace(ptr_v4_zone.name, "")[:-1]
@@ -113,10 +115,24 @@ class DnsUpdater(ConsumerMixin):
                     ])
                 if event_type == EVENT_DELETE:
                     log.info("Deleting {}".format(instancename))
+                    result = api.servers[0].search(instancename)
+                    fq_ptr_v4 = get_reverse_pointer(result[0]['content'])
+                    fq_ptr_v6 = get_reverse_pointer(result[1]['content'])
+                    ptr_v4_zone = self.suggested_zone(fq_ptr_v4)
+                    ptr_v6_zone = self.suggested_zone(fq_ptr_v6)
+                    ptr_v4_name = fq_ptr_v4.replace(ptr_v4_zone.name, "")[:-1]
+                    ptr_v6_name = fq_ptr_v6.replace(ptr_v6_zone.name, "")[:-1]
                     zone.delete_record([
                         powerdns.RRSet(hostname, 'A', []),
                         powerdns.RRSet(hostname, 'AAAA', []),
                     ])
+                    ptr_v4_zone.delete_record([
+                        powerdns.RRSet(ptr_v4_name, 'PTR', [])
+                    ])
+                    ptr_v6_zone.delete_record([
+                        powerdns.RRSet(ptr_v6_name, 'PTR', [])
+                    ])
+
 
 if __name__ == "__main__":
     log.info("Connecting to broker {}".format(BROKER_URI))
